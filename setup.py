599903456940bf0fe2e49605762808fb18b4c7ab
Grygoriy Gromko <ggromko@mail.ru>
2024-08-14 07:48:21 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -8,20 +8,18 @@ package_data = \
 {'': ['*']}
 
 install_requires = \
-['multipledispatch>=0.6.0,<0.7.0',
- 'numpy>=1.22.2,<2.0.0',
- 'pygame>=2.1.2,<3.0.0']
+['pygame>=2.1.2,<3.0.0','pycairo>=1.20.0']
 
 setup_kwargs = {
-    'name': 'p5',
-    'version': '0.0.8',
+    'name': 'p5-python',
+    'version': '0.6.29',
     'description': 'A package wrapping pygame functionality in p5.js calling conventions.',
     'long_description': '#p5 is a friendly tool for learning to code and make art. It is a free and open-source Python library built by an inclusive, nurturing community. p5 is a Python library that provides high level drawing functionality to help you quickly create simulations and interactive art using Python.',
-    'author': 'Grygoriy Gromko',,
+    'author': 'Grygoriy Gromko',
     'author_email': 'gr.gromko@gmail.com',
     'maintainer':  'Grygoriy Gromko',
     'maintainer_email': 'gr.gromko@gmail.com',
-    'url': 'https://github.com/gromko/p5js',
+    'url': 'https://github.com/gromko/p5-python',
     'packages': packages,
     'package_data': package_data,
     'install_requires': install_requires,
